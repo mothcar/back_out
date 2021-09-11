c2afb8f13e6a5425efe001d138c1911f8eb52f80
Paul Hwang <mothcar@gmail.com>
2021-09-11 13:36:10 +0900

    First Set

---
+++ src/views/Duplicate.vue
@@ -0,0 +1,32 @@
+<template>
+  <hello-world />
+</template>
+
+<script>
+
+  export default {
+    name: 'Home',
+
+    components: {
+    },
+
+    data() {
+      return{
+
+      }
+    }, // data
+
+    computed: {
+        // ...mapState('G_NAME', [])
+    }, // computed
+
+    mounted() {
+
+    }, // mounted
+
+    methods: {
+
+    }, // methods
+
+  }
+</script>
